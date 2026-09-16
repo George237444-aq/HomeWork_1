0d1c6451a8ee4222f83b744a2c27fd0fa5a72f73
George237444-aq <yuri4rt@yandex.ru>
2026-09-16 06:28:56 +0300

    сделал дз с циклом for

--- src/Main.java
+++ src/Main.java
@@ -396,6 +396,82 @@ public class Main {
       }
 
 
+     // /////////////////////////////////////////////////////////////
+
+     //задача 1
+     for (int i = 1; i <= 10; i ++ ){
+      System.out.println(i);
+     }
+
+     //задача 2
+
+     for (int i = 10; i > 0; i -- ){
+      System.out.println(i);
+     }
+
+
+     //задача 3
+     for (int i = 0; i <= 17; i = i +2){
+      System.out.println(i);
+     }
+
+
+     //задача 4
+
+     for (int i = -10; i <= 10; i++ ){
+      System.out.println(i);
+     }
+
+
+     //задача 5
+     for (int i = 1904; i <= 2096; i = i + 4 ){
+      System.out.println(i + " год является високосным");
+     }
+
+     //задача 6
+
+     for (int i = 7; i <= 98; i = i + 7){
+      System.out.println(i);
+     }
+
+     //задача 7
+     //
+     for (int i = 1; i <= 512; i = i * 2){
+
+      System.out.println(i);
+     }
+
+
+     //задача 8
+     int salary = 29000;
+     int total = 0;
+     for (int i = 0; i <= 12; i++) {
+      total = total + salary;
+     System.out.println("Месяц " + i + " Сумма накоплений равна " + total + " рубдей");
+    }
+
+
+     //задание 9
+      salary = 29000;
+      total = 0;
+     for (int i = 0; i <= 12; i++) {
+      total = total + total/100*12;
+      total = total + salary;
+      System.out.println("Месяц " + i + " Сумма накоплений равна " + total + " рубдей");
+     }
+
+
+     //final
+
+
+     for (int i = 0; i <= 10; i++){
+      int answer = 2 * i;
+      System.out.println("2*" + i + "=" + answer);
+     }
+
+
+
+
 
     }
 }
