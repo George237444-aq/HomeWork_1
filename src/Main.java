ea9728ec4875de09dbac2a263e180ffb927f0a43
George237444-aq <yuri4rt@yandex.ru>
2026-09-17 21:44:21 +0300

    все переделал

--- src/Main.java
+++ src/Main.java
@@ -401,17 +401,19 @@ public class Main {
 
 
      //задача 3
+
+
      for (int i = 0; i <= 17; i = i +2){
       System.out.println(i);
      }
 
 
+
      //задача 4
 
-     for (int i = 10; i <= -10; i++ ){
+     for (int i = 10; i >= -10; i-- ){
       System.out.println(i);
      }
-
 
      //задача 5
      for (int i = 1904; i <= 2096; i = i + 4 ){
